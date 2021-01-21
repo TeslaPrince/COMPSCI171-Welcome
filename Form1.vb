e01dfaf0b9ad256aea910fedd5059f3df1983970
Jenna Lambert <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 01:24:02 -0600

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btndisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        lblMsg.Text = "Welcome to Visual Basic!"
+        If lblMsg.Visible = True Then
+            lblMsg.Visible = False
+        ElseIf lblMsg.visible = False Then
+            lblMsg.Visible = True
+        End If
+    End Sub
+End Class
